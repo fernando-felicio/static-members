ed27d95a5835a1f6cf09da9a7320e53d8c01db0e
Fernando Felício <fernandofelicio.dev@gmail.com>
2023-08-03 09:40:09 -0300

    Criando metodos toConversion e showResult

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,28 @@
 package application;
 
+import java.util.Scanner;
+
+import java.util.Locale;
+
+import entities.CurrencyConverter;
+
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//Recebendo inputs do usuario
+		System.out.println("What is the dollar price? ");
+		CurrencyConverter.price = sc.nextDouble();
+		
+		System.out.println("How many dollars will be bought? ");
+		CurrencyConverter.qtd = sc.nextDouble();
+		
+		CurrencyConverter.showResult();
+		
+		sc.close();
 
 	}
 
--- src/entities/CurrencyConverter.java
+++ src/entities/CurrencyConverter.java
@@ -1,5 +1,22 @@
 package entities;
 
 public class CurrencyConverter {
+	
+	//Declarando os atributos da classe
+	
+	public static double price;
+	public static double qtd;
+	
+	
+	//Criando metodo de conversão
+	public static double toConversion() {
+		double totalConversion = (price * qtd) + (price * qtd * 0.06);
+		return totalConversion;
+	}
+	
+	//Criando metodo de retorno das informações
+	public static void showResult() {
+		System.out.printf("Amount to be paid in reais = R$%.2f%n", toConversion());
+	}
 
 }
